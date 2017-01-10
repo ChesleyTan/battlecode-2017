a796fba3c1d31081d65d346eeb220bd32f174a31
Brian Guo <brianguosh@gmail.com>
2017-01-09 23:43:38 -0500

    Added classes

---
+++ src/v1/RobotPlayer.java
@@ -0,0 +1,22 @@
+package v1;
+
+import battlecode.common.*;
+
+public class RobotPlayer{
+	
+	public static void run(RobotController rc) throws Exception{
+		switch(rc.getType()){
+		case ARCHON:
+			//Run Archon
+		case GARDENER:
+			//Run Gardener
+		case SCOUT:
+			//Run Scout
+		case SOLDIER:
+			//Run Soldier
+		case TANK:
+			//Run Tank
+		break;
+		}
+	}
+}
