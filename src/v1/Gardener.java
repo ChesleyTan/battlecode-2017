5509acb151fd4e99056f0b90d2207cd84060e584
Brian Guo <brianguosh@gmail.com>
2017-01-10 18:01:51 -0500

    gardeners moving around

--- src/v1/Gardener.java
+++ src/v1/Gardener.java
@@ -6,57 +6,91 @@ public class Gardener extends Globals{
 	
 	private static MapLocation[] otherArchonLoc;
 	private static int numTreesBuilt = 0;
+	private static Direction lastDir = null;
+	private static float detectRadius = 3f;
 	
-	public static void loop() throws GameActionException{
-		while(!rc.onTheMap(here.translate(0.001f, 0.001f), 2f)){
+	public static void checkspace() throws GameActionException{
+		while(!rc.onTheMap(here.translate(0.001f, 0.001f), detectRadius) || rc.isCircleOccupiedExceptByThisRobot(here.translate(0.001f, 0.001f), detectRadius)){ 
 			Globals.update();
-			if (!rc.onTheMap(here.add(Direction.getSouth(), 2f))){
-				if(rc.canMove(Direction.getNorth())){
-					rc.move(Direction.getNorth());
+			if (lastDir != null && rc.canMove(lastDir)){
+				rc.move(lastDir);
+				Clock.yield();
+			}
+			float[] volatileDirections = new float[10];
+			int index = 0;
+			if (!rc.onTheMap(here.add(Direction.getSouth(), detectRadius))){
+				volatileDirections[index] = Direction.getSouth().getAngleDegrees();
+				index ++;
+			}
+			else if (!rc.onTheMap(here.add(Direction.getEast(), detectRadius))){
+				volatileDirections[index] = Direction.getEast().getAngleDegrees();
+				index ++;
+			}
+			else if (!rc.onTheMap(here.add(Direction.getNorth(), detectRadius))){
+				volatileDirections[index] = Direction.getNorth().getAngleDegrees();
+				index ++;
+			}
+			else if (!rc.onTheMap(here.add(Direction.getWest(), detectRadius))){
+				volatileDirections[index] = Direction.getWest().getAngleDegrees();
+				index ++;
+			}
+		
+			RobotInfo[] nearbyRobots = rc.senseNearbyRobots(detectRadius);
+			if (nearbyRobots != null && nearbyRobots.length > 0 && !rc.hasMoved()){
+				for(int i = 0; i < nearbyRobots.length; i++){
+					if (nearbyRobots[i] != null){
+						volatileDirections[index] = here.directionTo(nearbyRobots[i].location).getAngleDegrees();
+						index++;
+					}
 				}
 			}
-			else if (!rc.onTheMap(here.add(Direction.getEast(), 2f))){
-				if(rc.canMove(Direction.getWest())){
-					rc.move(Direction.getWest());
-				}
-			}
-			else if (!rc.onTheMap(here.add(Direction.getNorth(), 2f))){
-				if(rc.canMove(Direction.getSouth())){
-					rc.move(Direction.getSouth());
-				}
-			}
-			else if (!rc.onTheMap(here.add(Direction.getWest(), 2f))){
-				if(rc.canMove(Direction.getEast())){
-					rc.move(Direction.getEast());
-				}
-			}
-		}
-		while(rc.isCircleOccupiedExceptByThisRobot(here.translate(0.001f, 0.001f), 3) && !rc.hasMoved()){
-			Globals.update();
-			RobotInfo[] nearbyRobots = rc.senseNearbyRobots(3);
-			if (nearbyRobots != null && nearbyRobots.length > 0){
-				System.out.println("true"); 
-				RobotInfo first = nearbyRobots[0];
-				Direction movedir = here.directionTo(first.location).opposite();
-				if (rc.canMove(movedir)){
-					rc.move(movedir);
-				}
-			}
-			TreeInfo[] nearbyTrees = rc.senseNearbyTrees(2);
+			TreeInfo[] nearbyTrees = rc.senseNearbyTrees(detectRadius);
 			if (nearbyTrees != null && nearbyTrees.length > 0){
-				TreeInfo first = nearbyTrees[0];
-				Direction movedir = here.directionTo(first.location).opposite();
-				if (rc.canMove(movedir)){
-					rc.move(movedir);
+				for(int i = 0; i < nearbyTrees.length; i++){
+					if (nearbyTrees[i] != null){
+						volatileDirections[index] = here.directionTo(nearbyTrees[i].location).getAngleDegrees();
+						index++;
+					}
 				}
 			}
+			float sum = 0;
+			for(int i = 0; i < index; i++){
+				sum += volatileDirections[i];
+			}
+			sum = sum / index;
+			sum = sum / 180 * (float)(Math.PI);
+			Direction movedir = new Direction(sum);
+			if(rc.canMove(movedir)){
+				rc.move(movedir);
+				lastDir = movedir;
+			}
+			else if (rc.canMove(movedir.opposite())){
+				rc.move(movedir.opposite());
+				lastDir = movedir;
+			}
+			else{
+				float rand = (float)(Math.random() * 2 * Math.PI);
+				Direction newdir = new Direction(rand);
+				if (rc.canMove(newdir)){
+					rc.move(newdir);
+					lastDir = newdir;
+				}	
+			}
+			Clock.yield();
 		}
+	}
+	public static void loop() throws GameActionException{
+		checkspace();
 		while(true){
 			try{
 				Globals.update();
 				if (numTreesBuilt < 6 && rc.canPlantTree(Direction.getNorth().rotateLeftDegrees(60 * numTreesBuilt))){
 					rc.plantTree(Direction.getNorth().rotateLeftDegrees(60*numTreesBuilt));
 					numTreesBuilt++;
+				}
+				else if (numTreesBuilt == 0){
+					checkspace();
+					Clock.yield();
 				}
 				/*else{
 					if(rc.canBuildRobot(RobotType.SCOUT, Direction.getNorth().rotateRightDegrees(60))){
