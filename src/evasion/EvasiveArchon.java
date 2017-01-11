003296dbf0529ced3e59b2b222d14b036c6f48c0
Chesley Tan <chesleytan97@gmail.com>
2017-01-10 20:06:32 -0500

    Tune Archon evasion and implement workarounds for server bugs

--- src/evasion/EvasiveArchon.java
+++ src/evasion/EvasiveArchon.java
@@ -34,14 +34,16 @@ public strictfp class EvasiveArchon {
                         Direction enemyAngle = myLoc.directionTo(ri.location);
                         for (int angleIndex = 0; angleIndex < 12; ++angleIndex) {
                             float weightOffset = (50 * (180 - Math
-                                    .abs(enemyAngle.degreesBetween(angleDirections[angleIndex]))));
+                                    .abs(degreesBetween(enemyAngle, angleDirections[angleIndex]))));
                             directionWeights[angleIndex] -= weightOffset;
                             System.out.println("Enemy angle: " + enemyAngle.getAngleDegrees());
                             System.out.println("Weight for angle "
                                     + angleDirections[angleIndex].getAngleDegrees() + ":"
                                     + weightOffset);
-                            System.out.println("Degrees between: "
-                                    + Math.abs(enemyAngle.degreesBetween(angleDirections[angleIndex])));
+                            System.out.println("Radians between: " + Math
+                                    .abs(radiansBetween(enemyAngle, angleDirections[angleIndex])));
+                            System.out.println("Degrees between: " + Math
+                                    .abs(degreesBetween(enemyAngle, angleDirections[angleIndex])));
                         }
                     }
                 }
@@ -96,38 +98,34 @@ public strictfp class EvasiveArchon {
                         }
                     }
                 }
-                /*
                 System.out.println("minX: " + minX);
                 System.out.println("maxX: " + maxX);
                 System.out.println("minY: " + minY);
                 System.out.println("maxY: " + maxY);
-                */
                 // Avoid corners and edges
-                /*
-                if (minX != UNKNOWN && myLoc.x - minX < 5) {
+                if (minX != UNKNOWN && myLoc.x - minX < 10) {
                     for (int angleIndex = 4; angleIndex < 9; ++angleIndex) {
-                        directionWeights[angleIndex] -= 10000;
+                        directionWeights[angleIndex] -= 2000 * (10 - (myLoc.x - minX));
                     }
                 }
-                if (minY != UNKNOWN && myLoc.y - minY < 5) {
+                if (minY != UNKNOWN && myLoc.y - minY < 10) {
                     for (int angleIndex = 7; angleIndex < 12; ++angleIndex) {
-                        directionWeights[angleIndex] -= 10000;
+                        directionWeights[angleIndex] -= 2000 * (10 - (myLoc.y - minY));
                     }
                 }
-                if (maxX != UNKNOWN && maxX - myLoc.x < 5) {
+                if (maxX != UNKNOWN && maxX - myLoc.x < 10) {
                     for (int angleIndex = 0; angleIndex < 3; ++angleIndex) {
-                        directionWeights[angleIndex] -= 10000;
+                        directionWeights[angleIndex] -= 2000 * (10 - (maxX - myLoc.x));
                     }
                     for (int angleIndex = 10; angleIndex < 12; ++angleIndex) {
-                        directionWeights[angleIndex] -= 10000;
+                        directionWeights[angleIndex] -= 2000 * (10 - (maxX - myLoc.x));
                     }
                 }
-                if (maxY != UNKNOWN && maxY - myLoc.y < 5) {
+                if (maxY != UNKNOWN && maxY - myLoc.y < 10) {
                     for (int angleIndex = 1; angleIndex < 6; ++angleIndex) {
-                        directionWeights[angleIndex] -= 10000;
+                        directionWeights[angleIndex] -= 2000 * (10 - (maxY - myLoc.y));
                     }
                 }
-                */
                 for (int angleIndex = 0; angleIndex < 12; ++angleIndex) {
                     System.out.println("Angle: " + (angleIndex * 30) + ", Weight: "
                             + directionWeights[angleIndex]);
@@ -155,12 +153,29 @@ public strictfp class EvasiveArchon {
                             moveAngleIndex = angleIndex;
                         }
                     }
-                    directionWeights[moveAngleIndex] -= 999999;
+                    System.out.println("Round: " + RobotPlayer.rc.getRoundNum());
                     System.out.println(
                             "Trying to move in direction: " + angleDirections[moveAngleIndex]);
                     moved = RobotPlayer.tryMove(angleDirections[moveAngleIndex], 5, 3);
-                } while (!moved && ++attempts <= 3);
-
+                    RobotPlayer.rc.setIndicatorLine(myLoc,
+                            myLoc.add(angleDirections[moveAngleIndex], 1), 0, 255, 0);
+                    RobotPlayer.rc.setIndicatorLine(myLoc.add(angleDirections[moveAngleIndex], 2),
+                            myLoc.add(angleDirections[moveAngleIndex], 1.5f), 255, 0, 0);
+                    RobotPlayer.rc.setIndicatorDot(myLoc,
+                            0,
+                            0,
+                            0);
+                    for (int angleIndex = 0; angleIndex < 12; ++angleIndex) {
+                        RobotPlayer.rc.setIndicatorDot(myLoc.add(angleDirections[angleIndex]),
+                        (int)Math.max(-25500, directionWeights[angleIndex]) / (-100),
+                        (int)Math.max(-25500, directionWeights[angleIndex]) / (-100),
+                        (int)Math.max(-25500, directionWeights[angleIndex]) / (-100));
+                    }
+                    directionWeights[moveAngleIndex] -= 999999;
+                } while (!moved && ++attempts <= 12);
+                if (attempts > 12) {
+                    RobotPlayer.tryMove(Direction.getNorth(), 10, 18);
+                }
                 // Broadcast archon's location for other robots on the team to know
                 RobotPlayer.rc.broadcast(0, (int) myLoc.x);
                 RobotPlayer.rc.broadcast(1, (int) myLoc.y);
@@ -172,6 +187,28 @@ public strictfp class EvasiveArchon {
                 System.out.println("Archon Exception");
                 e.printStackTrace();
             }
+
         }
+    }
+
+    public static float radiansBetween(Direction a, Direction b) {
+        return reduce(b.radians - a.radians);
+    }
+
+    public static float degreesBetween(Direction a, Direction b) {
+        return (float) Math.toDegrees(radiansBetween(a, b));
+    }
+
+    // Internally used to keep angles in the range (-Math.PI,Math.PI]
+    private static float reduce(float rads) {
+        if (rads <= -Math.PI) {
+            int circles = (int) Math.ceil(-(rads + Math.PI) / (2 * Math.PI));
+            return rads + (float) (Math.PI * 2 * circles);
+        }
+        else if (rads > Math.PI) {
+            int circles = (int) Math.ceil((rads - Math.PI) / (2 * Math.PI));
+            return rads - (float) (Math.PI * 2 * circles);
+        }
+        return rads;
     }
 }
