0f8aab5ca14790bd692770bd159028de6255d8bc
Brian Guo <brianguosh@gmail.com>
2017-01-11 15:58:12 -0500

    scout moving

--- src/v1/Scout.java
+++ src/v1/Scout.java
@@ -7,9 +7,10 @@ public class Scout extends Globals{
   private static final int attack_start_channel = 500;
   private static final int defense_start_channel = 250;
   private static final int target_channel = 451;
-  private enum Mode {ROAM, ATTACK};
-  private static Mode current_mode = Mode.ROAM;
+  private String[] Modes = {"ROAM", "ATTACK"};
+  private static String current_mode = "ROAM";
   private static Direction direction;
+  private static final int keepaway_radius = 5;
   
   public static void dodge() throws GameActionException {
     BulletInfo[] nearbyBullets = rc.senseNearbyBullets();
@@ -65,39 +66,111 @@ public class Scout extends Globals{
     }
     else{
       RobotInfo enemy = enemies[0];
-      rc.broadcast(target_channel, 1);
+      rc.broadcast(target_channel, enemy.ID);
       rc.broadcast(target_channel + 1, (int)(enemy.location.x));
       rc.broadcast(target_channel + 2, (int)(enemy.location.y));
       dodge();
       MapLocation center = enemy.location;
       rc.fireSingleShot(here.directionTo(center));
-      current_mode = Mode.ATTACK;
+      current_mode = "ATTACK";
     }
   }
   
+  public static void engage(int target) throws GameActionException{
+    dodge();
+    RobotInfo targetRobot = rc.senseRobot(target);
+    System.out.println(target);
+    rc.broadcast(target_channel + 1, (int)targetRobot.location.x);
+    rc.broadcast(target_channel + 2, (int)targetRobot.location.y);
+    direction = here.directionTo(targetRobot.location);
+    if (!rc.hasMoved()){
+      float absolute_dist = (float)Math.sqrt(Math.pow(here.x - targetRobot.location.x, 2) + Math.pow(here.y - targetRobot.location.y, 2));
+      if (absolute_dist > keepaway_radius + RobotType.SCOUT.strideRadius){
+        if(rc.canMove(direction)){
+          rc.move(direction);
+        }
+      }
+      else{
+        Direction rotated20 = direction.opposite().rotateLeftDegrees(20);
+        MapLocation newLoc = targetRobot.location.add(rotated20, keepaway_radius);
+        if (rc.canMove(newLoc)){
+          rc.move(newLoc);
+        }
+        else{
+          rotated20 = direction.opposite().rotateRightDegrees(20);
+          newLoc = targetRobot.location.add(rotated20, keepaway_radius);
+          if (rc.canMove(newLoc)){
+            rc.move(newLoc);
+          }
+        }
+      }
+    }
+    rc.fireSingleShot(direction);
+  }
   
 	public static void loop() throws GameActionException {
-	  while(true){
-	    Globals.update();
-  	  if(current_mode == Mode.ROAM){
-  	    int target = rc.readBroadcast(target_channel);
-  	    if (target != 0){
-  	      current_mode = Mode.ATTACK;
-  	      int xLoc = rc.readBroadcast(target_channel + 1);
-  	      int yLoc = rc.readBroadcast(target_channel + 2);
-  	      Direction target_direction = here.directionTo(new MapLocation(xLoc, yLoc));
-  	      direction = target_direction;
-  	      dodge();
-  	      if(rc.canMove(direction) && !rc.hasMoved()){
-  	        rc.move(direction);
-  	      }
-  	      Clock.yield();
-  	    }
-  	    else{
-  	      alert();
-  	    }
+	  try{
+	    direction = new Direction((float)(Math.random() * 2 * Math.PI));
+  	  while(true){
+  	    Globals.update();
+    	  if(current_mode == "ROAM"){
+    	    int target = rc.readBroadcast(target_channel);
+    	    if (target != 0){
+    	      current_mode = "ATTACK";
+    	      int xLoc = rc.readBroadcast(target_channel + 1);
+    	      int yLoc = rc.readBroadcast(target_channel + 2);
+    	      Direction target_direction = here.directionTo(new MapLocation(xLoc, yLoc));
+    	      direction = target_direction;
+    	      dodge();
+    	      if(rc.canMove(direction) && !rc.hasMoved()){
+    	        rc.move(direction);
+    	      }
+    	    }
+    	    else{
+    	      alert();
+    	      if (!rc.hasMoved() && rc.canMove(direction)){
+    	        rc.move(direction);
+    	      }
+    	      else if (!rc.onTheMap(here.add(direction, RobotType.SCOUT.strideRadius)) || rc.senseNearbyRobots(2.5f, us) != null){
+    	        Direction newDir = direction.rotateRightDegrees(45);
+    	        direction = newDir;
+    	        if (!rc.hasMoved() && rc.canMove(direction)){
+    	          rc.move(direction);
+    	        }
+    	        //System.out.println(direction.getAngleDegrees());
+    	      }
+    	    }
+    	  }
+    	  else{
+    	    int target = rc.readBroadcast(target_channel);
+    	    if(rc.canSenseRobot(target)){
+    	      while(rc.canSenseRobot(target)){
+    	        engage(target);
+    	        Clock.yield();
+    	      }
+    	      rc.broadcast(target_channel, 0);
+            current_mode = "ROAM";
+            direction = new Direction((float)(Math.random() * 2 * Math.PI));
+    	    }
+    	    else{
+    	      if (rc.readBroadcast(target_channel) != 0){
+      	      int xLoc = rc.readBroadcast(target_channel + 1);
+              int yLoc = rc.readBroadcast(target_channel + 2);
+              Direction target_direction = here.directionTo(new MapLocation(xLoc, yLoc));
+              if(rc.canMove(target_direction)){
+                rc.move(target_direction);
+              }
+    	      }
+    	      else{
+    	        current_mode = "ROAM";
+    	        direction = new Direction((float)(Math.random() * 2 * Math.PI));
+    	      }
+    	    }
+    	  }
+    	  Clock.yield();
   	  }
-  	  
+	  }catch(Exception e){
+	    e.printStackTrace();
 	  }
 	}
 }
