404c8cb4ea9e8342bba5a3cb7dcef4cf7294f31c
Ryan Butler <thebutlah@gmail.com>
2017-01-11 13:51:04 -0600

    Added stack trace printing to LoggerUtils, made a dummy bot for testing
    + Added printStackTrace() to LoggerUtils to enable DEBUG sensitive stack
      traces.
    * Clarified some documentation in LoggerUtils
    + Added a RobotPlayer inside a new package called dummybot for basic
      testing purposes

--- src/utils/LoggerUtils.java
+++ src/utils/LoggerUtils.java
@@ -2,6 +2,8 @@ package utils;
 
 import v1.Globals;
 
+import java.io.PrintWriter;
+
 /**
  * Provides useful logging functionality to be used throughout the application.
  *
@@ -25,8 +27,8 @@ public class LoggerUtils {
   }
 
   /**
-   * Logs the message to the current active channel.
-   * Will do nothing if the logger has DEBUG = false
+   * Logs a message to the current active channel.
+   * Will do nothing if Globals.DEBUG = false
    * @param msg The message to output to the console
    */
   public static void log(String msg) {
@@ -34,13 +36,32 @@ public class LoggerUtils {
   }
 
   /**
-   * Logs the message to the current active channel.
-   * Will do nothing if the logger has DEBUG = false
+   * Logs a message to the specified channel.
+   * Will do nothing if Globals.DEBUG = false
    * OR if the channel specified is not the current active channel.
    * @param msg The message to output to the console
    */
   public static void log(String channel, String msg) {
     if (Globals.DEBUG && channel.equalsIgnoreCase(activeChannel)) System.out.println(msg);
+  }
+
+  /**
+   * Logs the stack trace of an exception to the current active channel.
+   * Will do nothing if Globals.DEBUG = false
+   * @param e The specified exception
+   */
+  public static void printStackTrace(Throwable e) {
+    if (Globals.DEBUG) e.printStackTrace();
+  }
+
+  /**
+   * Logs the stack trace of an exception to the specified channel.
+   * Will do nothing if Globals.DEBUG = false
+   * OR if the channel specified is not the current active channel.
+   * @param e The specified exception
+   */
+  public static void printStackTrace(String channel, Throwable e) {
+    if (Globals.DEBUG && channel.equalsIgnoreCase(activeChannel)) e.printStackTrace();
   }
 
 
