eefc8cc039d8ca5c18571231116bb3304dea31ec
Chesley Tan <chesleytan97@gmail.com>
2017-01-18 00:04:16 -0500

    Lumberjack optimizations

--- src/finalVersion/Lumberjack.java
+++ src/finalVersion/Lumberjack.java
@@ -4,72 +4,77 @@ import battlecode.common.*;
 import utils.RobotUtils;
 import utils.Globals;
 
-public class Lumberjack extends Globals{
-  
+public class Lumberjack extends Globals {
+
   private static Direction mydir;
   private static RobotInfo target;
   private static TreeInfo targetTree;
   private static final int NEUTRAL = 0;
   private static final int DEFENSE = 1;
   private static int mode;
-  
+
   public static TreeInfo[] getAllTrees() {
-    TreeInfo[] first = rc.senseNearbyTrees(-1, them);
-    TreeInfo[] second = rc.senseNearbyTrees(-1, Team.NEUTRAL);
-    TreeInfo[] both = new TreeInfo[first.length+second.length];
-    for(int i=0;i<first.length;i++){
-        both[i] = first[i];
+    TreeInfo[] union;
+    TreeInfo[] nearbyTrees = rc.senseNearbyTrees(-1);
+    int count = 0;
+    for (TreeInfo ti : nearbyTrees) {
+      if (ti.team == them || ti.team == Team.NEUTRAL) {
+        ++count;
+      }
     }
-    for(int i=0;i<second.length;i++){
-        both[first.length + i] = second[i];
+    union = new TreeInfo[count];
+    for (TreeInfo ti : nearbyTrees) {
+      if (ti.team == them || ti.team == Team.NEUTRAL) {
+        union[--count] = ti;
+      }
     }
-    return both;
+    return union;
   }
-  
-  public static RobotInfo[] siftForLumbers(RobotInfo[] sample){
-    RobotInfo[] result = {};
-    int index = 0;
-    for (RobotInfo r: sample){
-      if(r.getType() == RobotType.LUMBERJACK){
-        RobotInfo[] current = result;
-        result = new RobotInfo[index + 1];
-        for (int i = 0; i < current.length; i++){
-          result[i] = current[i];
-        }
-        result[index] = r;
-        index++;
+
+  public static RobotInfo[] siftForLumbers(RobotInfo[] sample) {
+    RobotInfo[] result;
+    int count = 0;
+    for (RobotInfo ri : sample) {
+      if (ri.type == RobotType.LUMBERJACK) {
+        ++count;
+      }
+    }
+    result = new RobotInfo[count];
+    for (RobotInfo ri : sample) {
+      if (ri.type == RobotType.LUMBERJACK) {
+        result[--count] = ri;
       }
     }
     return result;
   }
 
-  public static void roam() throws GameActionException{
-    if (rc.canMove(mydir)){
+  public static void roam() throws GameActionException {
+    if (rc.canMove(mydir)) {
       rc.move(mydir);
     }
-    else{
-      while(!rc.canMove(mydir)){
+    else {
+      while (!rc.canMove(mydir)) {
         mydir = RobotUtils.randomDirection();
       }
-      if (rc.canMove(mydir)){
+      if (rc.canMove(mydir)) {
         rc.move(mydir);
       }
     }
   }
-  
-  public static void checkNearbyLumbersAndMove() throws GameActionException{
+
+  public static void checkNearbyLumbersAndMove() throws GameActionException {
     //Note: this assumes all nearby robots chasing the enemy are lumberjacks
-    if (rc.canSenseRobot(target.ID)){
+    if (rc.canSenseRobot(target.ID)) {
       target = rc.senseRobot(target.ID);
       RobotInfo[] attackingLumbers = siftForLumbers(rc.senseNearbyRobots(target.location, 4f, us));
       Direction toMe = target.location.directionTo(here);
       boolean isInRangeOfFriendlies = false;
       int rotateAmt = 0;
       MapLocation closestPoint = target.location.add(toMe, target.getRadius() + 1.1f);
-      for (RobotInfo r : attackingLumbers){
+      for (RobotInfo r : attackingLumbers) {
         //System.out.println(r.ID);
         //System.out.println(r.location.distanceTo(closestPoint));
-        if (r.location.isWithinDistance(closestPoint, 3)){
+        if (r.location.isWithinDistance(closestPoint, 3)) {
           isInRangeOfFriendlies = true;
           break;
         }
@@ -105,92 +110,94 @@ public class Lumberjack extends Globals{
       Direction principledirect = toMe.opposite();
       rc.setIndicatorDot(here.add(principledirect), 0, 255, 0);
       //System.out.println(target.ID);
-      if (!isInRangeOfFriendlies){
-        if (here.distanceTo(target.location) - RobotType.LUMBERJACK.bodyRadius - target.getRadius() > 3){
+      if (!isInRangeOfFriendlies) {
+        if (here.distanceTo(target.location) - RobotType.LUMBERJACK.bodyRadius
+            - target.getRadius() > 3) {
           boolean canMove = RobotUtils.tryMove(principledirect, 15, 6);
-          if (!canMove){
+          if (!canMove) {
             target = null;
           }
         }
-        else{
-          if (rc.canMove(principledirect)){
+        else {
+          if (rc.canMove(principledirect)) {
             rc.move(principledirect);
           }
         }
       }
-      else{
+      else {
         RobotUtils.tryMove(toMe, 15, 6);
         target = null;
       }
     }
-    else{
+    else {
       target = null;
     }
-    
-    
+
   }
-  public static void chase() throws GameActionException{
+
+  public static void chase() throws GameActionException {
     checkNearbyLumbersAndMove();
-    if (target != null){
-      if (here.distanceTo(target.location) <= 2 + target.getRadius()){
+    if (target != null) {
+      if (here.distanceTo(target.location) <= 2 + target.getRadius()) {
         rc.strike();
       }
-      if (target.getHealth() <= 0){
+      if (target.getHealth() <= 0) {
         target = null;
       }
     }
   }
-  
-  public static TreeInfo reachable(RobotInfo target){
+
+  public static TreeInfo reachable(RobotInfo target) {
     Direction toThere = here.directionTo(target.location);
     TreeInfo[] nearbyTrees = getAllTrees();
-    for (TreeInfo t : nearbyTrees){
+    for (TreeInfo t : nearbyTrees) {
       Direction toTree = here.directionTo(t.getLocation());
-      if (Math.abs(toThere.degreesBetween(toTree)) < 90){
+      if (Math.abs(toThere.degreesBetween(toTree)) < 90) {
         float distToTree = here.distanceTo(t.location) - t.getRadius();
         float distToTarget = here.distanceTo(target.location) - target.getRadius();
-        if (distToTree < distToTarget){
+        if (distToTree < distToTarget) {
           return t;
         }
       }
     }
     return null;
   }
-  
-  public static RobotInfo reachable(TreeInfo target){
+
+  public static RobotInfo reachable(TreeInfo target) {
     Direction toThere = here.directionTo(target.location);
     RobotInfo[] nearbyRobots = rc.senseNearbyRobots(-1, them);
-    for (RobotInfo r : nearbyRobots){
+    for (RobotInfo r : nearbyRobots) {
       Direction toTree = here.directionTo(r.getLocation());
-      if (Math.abs(toThere.degreesBetween(toTree)) < 90){
+      if (Math.abs(toThere.degreesBetween(toTree)) < 90) {
         float distToTree = here.distanceTo(r.location) - r.getRadius();
         float distToTarget = here.distanceTo(target.location) - target.getRadius();
-        if (distToTree < distToTarget){
+        if (distToTree < distToTarget) {
           return r;
         }
       }
     }
     return null;
   }
-  
-  public static void switchTarget(TreeInfo tree){
+
+  public static void switchTarget(TreeInfo tree) {
     target = null;
     targetTree = tree;
   }
-  
-  public static void switchTarget(RobotInfo robot){
+
+  public static void switchTarget(RobotInfo robot) {
     target = robot;
     targetTree = null;
   }
-  
-  public static void tryChop() throws GameActionException{
-    if (rc.canSenseTree(targetTree.ID)){
+
+  public static void tryChop() throws GameActionException {
+    if (rc.canSenseTree(targetTree.ID)) {
       targetTree = rc.senseTree(targetTree.ID);
       float distanceBetween = here.distanceTo(targetTree.location);
-      if(distanceBetween - RobotType.LUMBERJACK.bodyRadius - targetTree.getRadius() > RobotType.LUMBERJACK.strideRadius){
+      if (distanceBetween - RobotType.LUMBERJACK.bodyRadius
+          - targetTree.getRadius() > RobotType.LUMBERJACK.strideRadius) {
         Direction towardsTree = here.directionTo(targetTree.location);
         boolean moved = RobotUtils.tryMove(towardsTree, 15, 3);
-        if (!moved){
+        if (!moved) {
           targetTree = null;
         }
         /*if (rc.canMove(towardsTree)){
@@ -209,113 +216,113 @@ public class Lumberjack extends Globals{
           }
         }*/
       }
-      if (targetTree != null){
-        if(rc.canChop(targetTree.location)){
+      if (targetTree != null) {
+        if (rc.canChop(targetTree.location)) {
           rc.chop(targetTree.location);
         }
         //System.out.println("Health " + targetTree.getHealth());
       }
     }
-    else{
+    else {
       targetTree = null;
     }
   }
-  
-  public static RobotInfo priority(RobotInfo[] enemies){
+
+  public static RobotInfo priority(RobotInfo[] enemies) {
     RobotInfo result = null;
     int currvalue = 0;
-    for (RobotInfo r : enemies){
+    for (RobotInfo r : enemies) {
       int value = 0;
-      switch(r.getType()){
-      case GARDENER:
-        value = 4;
-        break;
-      case SCOUT:
-        value = 3;
-        break;
-      case ARCHON:
-        value = 2;
-        break;
-      case SOLDIER:
-        value = 1;
-        break;
-      case LUMBERJACK:
-        value = 1;
-        break;
-      case TANK:
-        value = 0;
-        break;
+      switch (r.getType()) {
+        case GARDENER:
+          value = 4;
+          break;
+        case SCOUT:
+          value = 3;
+          break;
+        case ARCHON:
+          value = 2;
+          break;
+        case SOLDIER:
+          value = 1;
+          break;
+        case LUMBERJACK:
+          value = 1;
+          break;
+        case TANK:
+          value = 0;
+          break;
       }
       //System.out.println("Value: " + value);
-      if( value > currvalue){
+      if (value > currvalue) {
         currvalue = value;
         result = r;
       }
     }
     return result;
   }
-  
-  public static void loop() throws GameActionException{
-    try{
-      if(currentRoundNum < 500){
+
+  public static void loop() throws GameActionException {
+    try {
+      if (currentRoundNum < 500) {
         mode = DEFENSE;
       }
-      else{
+      else {
         mode = NEUTRAL;
       }
-      mydir = new Direction((float)(rand.nextFloat() * 2 * Math.PI));
-      while(true){
+      mydir = new Direction((float) (rand.nextFloat() * 2 * Math.PI));
+      while (true) {
         Globals.update();
         // Consider targets that are behind trees
-        if (target != null || targetTree != null){
-          if (target != null){
+        if (target != null || targetTree != null) {
+          if (target != null) {
             TreeInfo closerTree = reachable(target);
-            if (closerTree == null){
+            if (closerTree == null) {
               chase();
             }
-            else{
+            else {
               switchTarget(closerTree);
               tryChop();
             }
           }
-          else if (targetTree != null){
+          else if (targetTree != null) {
             RobotInfo closerRobot = reachable(targetTree);
-            if (closerRobot == null){
+            if (closerRobot == null) {
               tryChop();
             }
-            else{
+            else {
               switchTarget(closerRobot);
               chase();
             }
           }
         }
-        else{
+        else {
           RobotInfo[] enemies = rc.senseNearbyRobots(-1, them);
-          if(enemies.length != 0){
+          if (enemies.length != 0) {
             target = priority(enemies);
             TreeInfo closerTree = reachable(target);
-            if (closerTree == null){
+            if (closerTree == null) {
               chase();
             }
-            else{
+            else {
               switchTarget(closerTree);
               tryChop();
             }
           }
-          else{
+          else {
             TreeInfo[] trees = getAllTrees();
-            if(trees.length != 0){
+            if (trees.length != 0) {
               targetTree = trees[0];
               RobotInfo closerRobot = reachable(targetTree);
-              if (closerRobot == null){
+              if (closerRobot == null) {
                 tryChop();
               }
-              else{
+              else {
                 switchTarget(closerRobot);
                 chase();
               }
             }
-            else{
+            else {
               roam();
             }
           }
@@ -324,7 +331,7 @@ public class Lumberjack extends Globals{
         RobotUtils.shakeNearbyTrees();
         Clock.yield();
       }
-    }catch(GameActionException e){
+    } catch (GameActionException e) {
       e.printStackTrace();
       Clock.yield();
     }
